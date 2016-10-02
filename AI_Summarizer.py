c68b383b82ee89e7bd7746ea666074f4749d873a
Czaremba <czaremba101@gmail.com>
2016-10-02 01:30:53 -0400

    Add files via upload

---
+++ AI_Summarizer.py
@@ -0,0 +1,21 @@
+#A Simple script that implements Machine learning algorithms to summarize text
+
+import Algorithmia
+
+
+input = open('Input.txt', 'r').read()
+client = Algorithmia.client('sim+fiwkxhdy48CoJcu1EonLOI61')
+algo = client.algo('nlp/Summarizer/0.1.3')
+result = algo.pipe(input) 
+
+print(result.result)   
+
+
+
+
+
+
+
+
+
+
